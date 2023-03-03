58dbe4f8cdb00b45eb41b844379d7906cfca5c80
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-03-03 10:48:13 -0300

    Start Project

---
+++ src/main/java/com/rodrigo/aluguel_carro/AluguelCarroApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.aluguel_carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelCarroApplication.class, args);
+	}
+
+}
